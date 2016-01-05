7e0d56c3fc5ffbd723ee09567f82574e803cff7d
vegim qehaja <med.batt.uk@gmail.com>
2016-01-05 16:46:24 +0000

    Weather class tests added

--- spec/AirportJavascriptSpec.js
+++ spec/AirportJavascriptSpec.js
@@ -3,28 +3,41 @@ describe('Airport', function() {
   var plane;
   var weather;
 
-  beforeEach(function() {
+  beforeAll(function() {
     airport = new Airport();
     weather = new Weather();
   });
 
-  it('allows a plane to land', function() {
-    expect(airport.land(plane)).toEqual('Plane Has Landed!');
+  describe('When Sunny Weather', function() {
+    beforeEach(function() {
+      spyOn(Math, 'random').and.returnValue(0.2);
+    });
+    
+    it('allows a plane to land', function() {
+      expect(airport.land(plane)).toEqual('Plane Has Landed!');
+    });
+
+    it('allows a plane to take off', function() {
+      expect(airport.takeOff(plane)).toEqual('Plane Has Taken Off!');
+    });
+
+    it('has a capacity of 20', function() {
+     expect(airport.capacity).toEqual(20);
+    });
   });
 
-  it('allows a plane to take off', function() {
-    expect(airport.takeOff(plane)).toEqual('Plane Has Taken Off!');
-  });
+  describe('When Stormy weather', function() {
+    beforeEach(function() {
+      spyOn(Math, 'random').and.returnValue(0.9);
+    });
 
-  it('has a capacity of 20', function() {
-   expect(airport.capacity).toEqual(20);
-  });
+    it('prevent takeoff', function() {
+      expect(airport.weatherCheck()).toEqual('Stormy');
+    });
 
-  it('prevent takeoff when weather is stormy', function() {
-  spyOn(Math, 'random').and.returnValue(0.9);
-
-  expect(airport.weatherCheck()).toEqual('Stormy');
-  //  expect(airport.takeOff).toThrow(new Error('Stormy weather cannot takeoff'));
+    it('prevent landing', function() {
+      expect(airport.weatherCheck()).toEqual('Stormy');
+    });
   });
 
 });
