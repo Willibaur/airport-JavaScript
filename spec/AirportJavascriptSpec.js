395df8bcc0172405b3eee4e249e51b818ac6b059
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-05 13:39:01 +0000

    initial commit: JavaScript suite set up

---
+++ spec/AirportJavascriptSpec.js
@@ -0,0 +1,25 @@
+describe('Airport', function() {
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+  });
+
+  it('allows a plane to land', function() {
+    expect(airport.land(plane)).toEqual('Plane Has Landed!')
+  });
+
+  // it('', function() {
+
+  // });
+
+  // it('', function() {
+
+  // });
+
+  // it('', function() {
+
+  // });
+  
+});
